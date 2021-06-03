29599a029287fa87b6ad8970d3137a18c172f4ee
Snjezana Simic <s.snjezana@outlook.com>
2021-06-03 14:45:00 +0200

    data for cooper

--- run/rwm/rwm/src/main/resources/data.sql
+++ run/rwm/rwm/src/main/resources/data.sql
@@ -9,7 +9,26 @@ insert into "users" ( name, surname, email, verified,username, password,user_rol
 insert into "users" (name, surname, email, verified,username, password,user_role,training_plan,runner_data)
  values ('Snjezana','Simic', 's.snjezana@gmail.com',true,'user1', '$2a$10$RVzuprKddsjdq6P8QWmqF.sCj2uYPIUlbFVB.b7tJ9RdFNOOBNoXO' ,'ROLE_RUNNER',null, null);
 
+
 insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
-                values(1, 20, 29, 1500, 1800, 2200, 2700)
+                values(0, 20, 30, 1600, 2200, 2400, 2800);
+insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
+                values(0, 30, 40, 1500, 1900, 2300, 2700);
+insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
+                values(0, 40, 50, 1400, 1700, 2100, 2500);
+insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
+                values(0, 50, 110, 1300, 1600, 2000, 2400);
+
+
+insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
+                values(1, 20, 30, 1500, 1800, 2200, 2700);
+insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
+                values(1, 30, 40, 1400, 1700, 2000, 2500);
+insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
+                values(1, 40, 50, 1200, 1500, 1900, 2300);
+insert into cooper_table (gender, age_min, age_max, very_bad, bad, average, good)
+                values(1, 50, 110, 1100, 1400, 1700, 2200);
+
+
 
 
